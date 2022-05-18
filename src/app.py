19e8ef3215f8cbbad831c1edd75daf97cf1bd8fd
Jarlinton Moreno Zea <jarlintonmorenozea@Jarlintons-MBP.domain.name>
2022-05-18 09:21:45 -0500

    Removed unnecessary comments and "No matches found" validation

--- src/app.py
+++ src/app.py
@@ -7,17 +7,14 @@ def find_pairs(nba_players: list, height_in: int = 0):
     """
 
     matches = dict()
-    """
-        val: index -> 'h_in' 
-        val: first_name + last_name
-        index: index on nba_players
-        {'id_player': 'h_in'}
-    """
+
+    counter_matching_pairs: int = 0
 
     for id_player, player in enumerate(nba_players):
         diff = str(height_in - int(player["h_in"]))
 
         if diff in matches.values():
+            counter_matching_pairs += 1
             # getting the idx for the pair_player on nba_players
             sec_id_player = int(
                 list(matches.keys())[list(matches.values()).index(diff)]
@@ -30,14 +27,14 @@ def find_pairs(nba_players: list, height_in: int = 0):
 
         matches[str(id_player)] = player["h_in"]
 
-    # print(matches.values())
-    # print(nba_players)
+    if counter_matching_pairs == 0:
+        print("No matches found")
 
 
 if __name__ == "__main__":
 
     try:
-        HEIGHT = 139
+        HEIGHT = 119
         END_POINT_URL = "https://mach-eight.uc.r.appspot.com/"
 
         response = requests.get(END_POINT_URL)
